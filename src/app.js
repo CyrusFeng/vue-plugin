e452084480f6108a77e1ca789842ebe9dc726ecb
sizeof <625327545@qq.com>
2019-01-01 21:32:04 +0800

    使用chai、mocha做单元测试，Karma做自动化测试

--- src/app.js
+++ src/app.js
@@ -1,16 +1,94 @@
 import Vue from 'vue'
 import Button from './button.vue'
-import Icon from './g-icon'
 import ButtonGroup from './g-button-group'
 
 Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
+
+// const Constructor = Vue.extend(Button)
+// const button = new Constructor({
+//   propsData: {
+//     icon: 'settings'
+//   }
+// })
+// button.$mount('#app')
+
+// const vm = new Constructor({
+//   propsData: {
+//     icon: 'settings'
+//   }
+// })
+
 
 new Vue({
   el:'#app',
   data:{
     message:'hi vue',
-    classname:undefined
   }
 })
+
+// import chai from 'chai'
+// const expect = chai.expect
+//
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button = new Constructor({
+//     propsData:{
+//       iconName:'settings'
+//     }
+//   })
+//   button.$mount()
+//   let useElement = button.$el.querySelector('use')
+//   let href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-settings')
+// }
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button = new Constructor({
+//     propsData:{
+//       iconName:'settings',
+//       loading:true
+//     }
+//   })
+//   button.$mount('')
+//   let useElement = button.$el.querySelector('use')
+//   let href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-loading')
+// }
+// {
+//   const div = document.createElement('div')
+//   document.body.appendChild(div)
+//   const Constructor = Vue.extend(Button)
+//   const button = new Constructor({
+//     propsData:{
+//       iconName:'settings',
+//       loading:true,
+//       iconPosition:'right'
+//     }
+//   })
+//   button.$mount(div)
+//   let svg = button.$el.querySelector('svg')
+//   let {order} = window.getComputedStyle(svg)
+//   expect(order).to.eq('2')
+//   button.$el.remove()
+//   button.$destroy()
+// }
+
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button = new Constructor({
+//     propsData:{
+//       iconName:'settings',
+//       loading:true,
+//       iconPosition:'right'
+//     }
+//   })
+//   button.$mount()
+//
+//   let btnDom = button.$el
+//   console.log(btnDom);
+//   btnDom.on('click',()=>{
+//     console.log(1)
+//   })
+//   btnDom.click()
+// }
--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,9 @@
 <template>
     <button class="g-button" :class="`icon-${iconPosition?iconPosition:undefined}`" @click="changeLoading">
-    <!--<button class="g-button" :class="{[`icon-${iconPosition}`]:true}">-->
-    <!--<button class="g-button" :class="iconPosition">-->
-        <g-icon v-show="iconName && !loading" :iconName="iconName"></g-icon>
-        <g-icon v-show="loading" class="loading" iconName="loading"></g-icon>
+        <!--<button class="g-button" :class="{[`icon-${iconPosition}`]:true}">-->
+        <!--<button class="g-button" :class="iconPosition">-->
+        <g-icon v-if="iconName && !loading" :iconName="iconName"></g-icon>
+        <g-icon v-if="loading" class="loading" iconName="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -11,30 +11,35 @@
 </template>
 
 <script>
+  // import Vue from 'vue'
+  import Icon from './g-icon'
+  // Vue.component('g-icon',Icon)
   export default {
+    components:{
+      'g-icon':Icon
+    },
     props: {
       iconName: {
         type: String,
       },
-      iconPosition:{
-        type:String,
-        default:'left',
-        validator(val){
-          console.log(val);
-          if(val !== 'left' && val !== 'right'){
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(val) {
+          if (val !== 'left' && val !== 'right') {
             return false
-          }else {
+          } else {
             return true
           }
         }
       },
-      loading:{
-        type:Boolean,
-        default:false
+      loading: {
+        type: Boolean,
+        default: false
       }
     },
-    methods:{
-      changeLoading(){
+    methods: {
+      changeLoading() {
         this.loading = !this.loading
       }
     }
@@ -42,14 +47,15 @@
 </script>
 
 <style scoped lang="scss">
-    @keyframes spin{
-        0%{
+    @keyframes spin {
+        0% {
             transform: rotate(0deg);
         }
-        100%{
+        100% {
             transform: rotate(360deg);
         }
     }
+
     .g-button {
         display: inline-flex;
         justify-content: center;
@@ -71,23 +77,22 @@
             outline: none;
         }
 
-        &.icon-left{
+        .icon {
+            order: 1;
+        }
+        .content {
+            order: 2;
+        }
+
+        &.icon-right {
             .icon {
-                order: 1;
-            }
-            .content{
                 order: 2;
             }
-        }
-        &.icon-right{
-            .icon {
-                order: 2;
-            }
-            .content{
+            .content {
                 order: 1;
             }
         }
-        .loading{
+        .loading {
             animation: spin 1s infinite linear;
         }
     }
