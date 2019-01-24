76eab3f9f26ffbc68f1eb2d1b360d9d31ae43bb3
sizeof <625327545@qq.com>
2019-01-25 00:09:14 +0800

    修复了点击弹出层后监听事件被移出的bug

--- src/g-popover.vue
+++ src/g-popover.vue
@@ -1,39 +1,75 @@
 <template>
-    <div class="wrapper" @click="addListener">
-        <div class="pop-wrapper" v-if="showPop">
+    <div class="wrapper" @click="addListener($event)" ref="wrapper">
+        <div class="pop-wrapper" ref="popWrapper" v-if="showPop">
             <slot name="pop"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrap" class="btn">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
 <script>
   export default {
     name: "g-popover",
+    created(){
+      this.showPop = false
+    },
     data(){
       return {
         showPop:false,
       }
     },
     methods:{
-      addListener(){
-        this.showPop = !this.showPop
+      addListener(e){
+        console.log('点击了组件')
+        if (this.$refs.triggerWrap.contains(e.target)){
 
-        if(this.showPop){
-          console.log('显示')
-          this.$nextTick(()=>{
-            document.addEventListener('click',this.hiddenPop,{
-              once : true
+          this.showPop = !this.showPop
+
+          if(this.showPop){
+
+            this.$nextTick(()=>{
+              document.body.appendChild(this.$refs.popWrapper)
+
+              // document.addEventListener('click',this.hiddenPop,{
+              //   once : true
+              // })
+              document.addEventListener('click',this.hiddenPop)
+
+              //将弹出层放在body中，以防用户在组件外使用overflow：hidden时弹出层被遮盖
+
+
+              let { width,height,top,left} = this.$refs.triggerWrap.getBoundingClientRect()
+
+              this.$refs.popWrapper.style.left = left+ window.scrollX+'px'
+              this.$refs.popWrapper.style.top = top+ window.scrollY +'px'
+
+              //document.removeEventListener('click',this.hiddenPop)
             })
-            //document.removeEventListener('click',this.hiddenPop)
-          })
+          }else{
+            console.log('点击按钮隐藏')
+          }
         }else{
-          console.log('隐藏')
+          console.log('没点击按钮')
         }
+
       },
-      hiddenPop(){
-        this.showPop = false
-        console.log('隐藏')
+      hiddenPop(e){
+        if (this.$refs.popWrapper.contains(e.target)) {
+            console.log('点击了pop')
+        }else{
+          console.log('没点击弹出层')
+          this.showPop = false
+          console.log('document隐藏')
+          document.removeEventListener('click',this.hiddenPop)
+          console.log('监听器消失')
+        }
+
+
+      },
+      xxx(){
+        console.log('haha')
       }
     }
   }
@@ -41,15 +77,22 @@
 
 <style scoped lang="scss">
     .wrapper{
+        padding: 20px;
         margin: 100px;
         display: inline-block;
         vertical-align: middle;
         position: relative;
-        .pop-wrapper{
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-        }
+        border: 1px solid red;
     }
+    .pop-wrapper{
+        position: absolute;
+        /*bottom: 100%;*/
+        /*left: 0;*/
+        transform: translateY(-100%);
+        border: 1px solid red;
+    }
+    .btn{
+        border: 1px solid red;
+    }
+
 </style>
